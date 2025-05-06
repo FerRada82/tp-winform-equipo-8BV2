d520494751a8a432372d82f0686dc5d86e3ef7cc
FerRada82 <fer_rada82@hotmail.com>
2025-05-06 00:29:05 -0300

    primera subida

---
+++ TPWinForm_equipo-8B-V2/Articulo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TPWinForm_equipo_8B_V2
+{
+    internal class Articulo
+    {
+        public int Id { get; set; }
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }        
+        public string Descripcion { get; set; }
+        public Marca Marca { get; set; }
+        public Categoria Categoria { get; set; }
+        public decimal Precio { get; set; }
+        public string UrlImagenes { get; set; }
+    }
+}
---
+++ TPWinForm_equipo-8B-V2/Marca.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TPWinForm_equipo_8B_V2
+{
+    internal class Marca
+    {
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
